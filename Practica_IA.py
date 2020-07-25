61c062fa568908bbf52875df534c4b4e22ee26b0
Joss <jos.regy@gmail.com>
2020-07-25 00:36:55 -0500

    version 3 ya merito queda :3

--- Practica_IA.py
+++ Practica_IA.py
@@ -1,7 +1,11 @@
 from tkinter import *
 import speech_recognition as sr
-#import subprocess
 import easygui
+import numpy as np
+from os import path
+import matplotlib.pyplot as plt
+import random
+from wordcloud import WordCloud, STOPWORDS
 
 def clicked_dictar():
     # Record Audio
@@ -25,20 +29,71 @@ def clicked_dictar():
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
+#metodo para crear el cuadro de texto para poder escribir
 def TextInput():
-    #se obtiene el texto ingresado en la caja de texto
-    texto_dictado = txtInput.get()
-    print(texto_dictado)
+    txtInput.configure(state="normal")
+
+#metodo para leer el texto de la caja de texto
+def leer_texto():
+    global textoIngresado
+    textoIngresado = txtInput.get('1.0', END).lower()
+    separar_palabras()
+
+#metodo para separar el texto en palabras
+def separar_palabras():
+    palabras = textoIngresado.replace('\n', ' ').split()
+    contar_palabras(palabras)
+
+#metodo para contar las palabras
+def contar_palabras(lista_palabras):
+    frecuenciaPalabras = []
+    for w in lista_palabras:
+        frecuenciaPalabras.append(lista_palabras.count(w))
+    
+    print("Pares\n" + str(list(zip(lista_palabras, frecuenciaPalabras))))
+
+    
+
+#metodo para generar el resumen del texto ingresado
+def resumen():
+    leer_texto()
+
+    GenResumen1 = Entry(window)
+    GenResumen2 = Entry(window)
+    GenResumen3 = Entry(window)
+
+    GenResumen1.grid(column=2, row=8)
+    GenResumen2.grid(column=2, row=9)
+    GenResumen3.grid(column=2, row=10)
+    
+    GenResumen1.insert(INSERT,"palabras: ")
+    GenResumen2.insert(INSERT,"palabras enlace: ")
+    GenResumen3.insert(INSERT,"palabras clave: ")
+    
+    GenResumen1.configure(state="disabled")
+    GenResumen2.configure(state="disabled")
+    GenResumen3.configure(state="disabled")
     
 
 def btnBuscar():
     #subprocess.Popen('Explorer /select,"C:Users/black/Documents/"')
     file = easygui.fileopenbox("C:Users/black/Documents/")
 
+def nubePalabras():
+    #text = 'Este recopilatorio adaptado por Mundo Primaria está pensado y desarrollado centrándose únicamente en el público infantil, por lo que hemos revisado, y preparado todas las historias que encontrarás aquí para que resulte asequible y sencillo de entender en los más pequeños y a su vez mantenga la atención y la intriga en los que son algo más mayores, incentivando así el hábito de la lectura desde la infancia.'
+    text = txtInput.get('1.0', END).lower()
+    wordcloud = WordCloud(font_path='C:/Windows/Fonts/Verdana.ttf',
+                            relative_scaling = 0.1,
+                            stopwords = {'que', 'y', 'en', 'el', 'por', 'las', 'a', 'la', 'de', 'lo', 'más', 'este'} # set or space-separated string
+                            ).generate(text)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show()
+
 window = Tk()
 window.title("Organizador de ideas")
 
-window.geometry('900x500')
+window.geometry('1000x500')
 
 etiqueta = Label(window, text="          ")
 etiqueta.grid(column=0, row=0)
@@ -57,12 +112,17 @@ btnChargeDoc.grid(column=4, row=1)
 btnDicInputText = Button(window, text="Dicta un texto", command=clicked_dictar)
 btnDicInputText.grid(column=2, row=3)
 
-
 global txtInput
 txtInput = Text(window)
 txtInput.config(width=90, height=10, font=("Consolas",10))
 txtInput.grid(column=3, row=4)
+txtInput.configure(state="disabled")
 
+btnResumen = Button(window, text="Generar resumen del texto anterior", command=resumen)
+btnResumen.grid(column=2, row=7)
+
+btnNubePalabras = Button(window, text="Generar nube de palabras", command=nubePalabras)
+btnNubePalabras.grid(column=4, row=7)
 
 window.mainloop()
 
